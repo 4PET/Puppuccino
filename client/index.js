b78510bfa844ac2a58dc7253626f715176e91a69
Talya Sasek <talyaercag@gmail.com>
2019-12-31 15:56:27 -0800

    fixed store import and missing default for redux

--- client/index.js
+++ client/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import styles from './stylesheets/style.scss';
 import App from './components/App.jsx';
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
+import store from './store.js'
 
 render(
     <Provider store={store}>
