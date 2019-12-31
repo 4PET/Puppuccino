dc8a3761f43c326dc51d48f61180c7990b464dfd
Talya Sasek <talyaercag@gmail.com>
2019-12-30 17:34:32 -0800

    Added login, signup, signout, accountpage, usercomponent and dogcomponent components

---
+++ client/components/Login.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Login = (props) => {
+  return (
+    <div id="loginContainer" className="container">
+      <form id="loginForm">
+        <h2>Log In</h2>
+        <input
+          type="text"
+          name="username"
+          autoComplete="username"
+          placeholder="Username"
+          onChange={e => props.updateInfo(e.target.name, e.target.value)}
+        />
+        <input
+          type="password"
+          name="password"
+          autoComplete="password"
+          placeholder="Password"
+          onChange={e => props.updateInfo(e.target.name, e.target.value)}
+        />
+        <button onClick={props.handleLogin}>Log In</button>
+        <div className="toggleSignup" onClick={props.handleToggleSignup}>
+          Click to Sign Up
+        </div>
+      </form>
+    </div >
+  );
+}
+
+export default Login;
---
+++ client/components/Signup.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Signup = props => {
+  return (
+    <div id="signupContainer" className="container">
+      <form id="signupForm">
+        <h2>Sign Up</h2>
+        <input
+          type="text"
+          name="username"
+          autoComplete="username"
+          placeholder="Username"
+          onChange={e => props.updateInfo(e.target.name, e.target.value)}
+        />
+        <input
+          type="password"
+          name="password"
+          autoComplete="password"
+          placeholder="Password"
+          onChange={e => props.updateInfo(e.target.name, e.target.value)}
+        />
+        <button onClick={props.handleSignup}>Sign Up</button>
+        <div className="toggleSignup" onClick={props.handleToggleSignup}>
+          Have an Account? Click to Log In
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
